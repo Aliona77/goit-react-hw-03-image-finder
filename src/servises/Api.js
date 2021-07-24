3a843a54e74d22f4e7467d504d3ad4e8e537046c
Aliona77 <chabanalona153@gmail.com>
2021-07-24 19:36:26 +0300

    виправлено помилки

--- src/servises/Api.js
+++ src/servises/Api.js
@@ -1,15 +1,14 @@
 import axios from 'axios';
 
-const baseURL = 'https://pixabay.com/api';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 const KEY = '21758242-df60cf310fb09062fe07e2a40';
 const per_page = 12;
 
-const fetchImages = (imageName, page = 1) => {
-  return axios
-    .get(
-      `${baseURL}?q=${imageName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`,
-    )
-    .then(response => response.data.hits);
+const fetchImages = async (imageName, page = 1) => {
+  const { data } = await axios.get(
+    `?q=${imageName}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=${per_page}`,
+  );
+  return data.hits;
 };
 
 const imagesApi = {
